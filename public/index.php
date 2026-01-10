b5889165af440560885108599c9d258d929aff6b
Maikl-k <guyfhrtbe6v67btrtjhrtj@gmail.com>
2026-01-10 13:30:24 +0200

    search bar frontend

--- public/index.php
+++ public/index.php
@@ -1,2 +1,17 @@
 <?php
 
+require_once  '../vendor/autoload.php';
+
+$route = new AltoRouter();
+
+$route->map('GET', '/', function(){
+    require '../src/views/home.php';
+}, 'home');
+
+$match = $route->match();
+
+if($match && is_callable($match['target'])) {
+    call_user_func_array($match['target'], $match['params']);
+}else{
+    header($_SERVER['SERVER_PROTOCOL'].'404 not found');
+}
---
+++ src/views/home.php
@@ -0,0 +1,28 @@
+<?php
+ $baseUrl = '/'; 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href=" <?php echo $baseUrl; ?>css/home.css">
+    <link rel="stylesheet" href=" <?php echo $baseUrl; ?>css/global.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dozvillia | Home page</title>
+</head>
+<body>
+    <div class="nav_section">
+        <div class="search-bar">
+            <form action="" method="GET" id="search-form">
+                <div class="form-items">
+
+                    <input type="search" name="seacher-content" id="search-bar" placeholder="type here for search">
+                    
+                    <input type="image" id="search-image" src=" <?php echo $baseUrl; ?>images/goodSearchBtn.png" alt="search button">
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
