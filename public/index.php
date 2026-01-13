6eb57032dbfc8306b26160d503a5cda21108d9a8
Maikl-k <guyfhrtbe6v67btrtjhrtj@gmail.com>
2026-01-13 12:56:26 +0200

    basic pages and routes

--- public/index.php
+++ public/index.php
@@ -8,15 +8,55 @@ $twig = new \Twig\Environment($loader, [
     'debug' => true,
 ]);
 
-$route = new AltoRouter();
+$router = new AltoRouter();
 
-$route->map('GET', '/', function() use($twig){
+//$route->map('GET', '/', function() use($twig){
     
-    echo $twig->render('home.html.twig');
+//    echo $twig->render('home.html.twig');
 
-}, 'home');
+//}, 'home');
 
-$match = $route->match();
+$routes = array(
+    array('GET', '/', function() use($twig){
+        echo $twig->render('home.html.twig');
+    }, 'home'),
+
+    array('GET', '/create-recomendation', function() use($twig){
+        echo $twig->render('create-recomendation.html.twig');
+    }, 'create-recomendation' ),
+
+    array('GET', '/about', function() use($twig){
+        echo $twig->render('about.html.twig');
+    }, 'about'),
+
+    array('GET', '/login', function()use($twig){
+        echo $twig->render('login.html.twig');
+    }, 'login'),
+
+    array('GET', '/sign-up', function() use($twig){
+        echo $twig->render('sign-up.html.twig');
+    }, 'sign-up'),
+
+    array('GET', '/weclome', function() use($twig){
+        echo $twig->render('welcome.html.twig');
+    }, 'welcome'),
+
+    array('GET', '/users/[*:user-name]', function() use($twig){
+        echo $twig->render('profile.html.twig');
+    }, 'user-profile'),
+
+    array('GET', '/items/[*:item-name]', function() use($twig){
+        echo $twig->render('item.html.twig');
+    }, 'item'),
+
+    array('GET', '/watch-recomendation', function() use($twig){
+        echo $twig->render('watch-recomendation.html.twig');
+    }, 'watch-recomendation'),
+);
+
+$router->addRoutes($routes);
+
+$match = $router->match();
 
 if($match && is_callable($match['target'])) {
     call_user_func_array($match['target'], $match['params']);
