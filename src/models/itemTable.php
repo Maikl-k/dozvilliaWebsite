777041372b647d25a1c3fddbdd0ccc6a726906f1
Maikl-k <guyfhrtbe6v67btrtjhrtj@gmail.com>
2026-03-03 14:36:41 +0200

    add items table to db

---
+++ src/models/itemTable.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once "database.php";
+
+$database = new DataBase();
+
+$itemTableSql = "CREATE TABLE IF NOT EXISTS Items(
+                    item_id INT AUTO_INCREMENT,
+                    creator_id INT NOT NULL,
+                    item_image blob NOT NULL,
+                    item_title VARCHAR(60) NOT NULL,
+                    item_section ENUM('movie', 'book', 'serial', 'podcast') NOT NULL,
+                    item_release_date DATE NOT NULL,
+                    item_description VARCHAR(2000) NOT NULL,
+                    item_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                    item_change_date DATETIME,
+                    Primary key (item_id, creator_id),
+                    foreign key (creator_id) references Users(user_id)
+                    );
+                    ";
+
+$stmt = $database->getConnection()->prepare($itemTableSql);
+
+$stmt->execute();
